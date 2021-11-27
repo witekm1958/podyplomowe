eeb3e9ffcbcd10dc65b2ccac41a3c20b756472b7
matusins <matusins@KOMP409-11>
2021-11-27 16:35:56 +0100

    Komituję po raz kolejny.

--- src/GardenersDog.java
+++ src/GardenersDog.java
@@ -1,5 +1,6 @@
 public class GardenersDog {
     public static void main(String[] args) {
         System.out.println("Pies ogrodnika nazywa się Anioł");
+        System.out.println("Ogrodnik lubi gruszki a pies nie.");
     }
 }
