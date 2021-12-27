4040d5c872770672208898ea0cf50afbb8c0ee00
witekm1958 <45516622+witekm1958@users.noreply.github.com>
2021-12-27 11:50:03 +0100

    Update test2.txt

--- src/PiesOgrodnika.java
+++ src/PiesOgrodnika.java
@@ -1,6 +1,8 @@
-public class GardenersDog {
+public class PiesOgrodnika {
     public static void main(String[] args) {
         System.out.println("Pies ogrodnika nazywa się Anioł");
         System.out.println("Ogrodnik lubi gruszki a pies nie.");
+        System.out.println("Pies szczeka na amatorów kwaśnych jabłek");
     }
+
 }
